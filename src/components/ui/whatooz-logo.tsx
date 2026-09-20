540b6179f6ac5ae8ffa5b4c941e8fdc12dc3a97f
stephdev12 <stephdev12@users.noreply.github.com>
2026-09-20 10:22:39 +0200

    chore: swap light and dark mode logos

--- src/components/ui/whatooz-logo.tsx
+++ src/components/ui/whatooz-logo.tsx
@@ -35,12 +35,12 @@ export function WhatoozLogo({
         )}
       >
         <img
-          src="/logo_noir.png"
+          src="/logo_white.png"
           alt="Whatooz"
           className="h-full w-full object-contain pointer-events-none transform scale-200 block dark:hidden"
         />
         <img
-          src="/logo_white.png"
+          src="/logo_noir.png"
           alt="Whatooz"
           className="h-full w-full object-contain pointer-events-none transform scale-200 hidden dark:block"
         />
